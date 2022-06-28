54b60666768d28320d00370237b07ac08bd5d613
abbat01 <95858072+abbat01@users.noreply.github.com>
2022-06-28 19:49:44 +0300

    init: fix text

---
+++ wheel/index.js
@@ -0,0 +1,21 @@
+
+let container = document.querySelector(".container");
+let btn = document.getElementById("spin");
+let number = Math.ceil(Math.random() * 2000);
+
+btn.onclick = function () {
+	container.style.transform = "rotate(" + number + "deg)";
+	number += Math.ceil(Math.random() * 2000);
+}
+
+
+
+let modal = document.querySelector('.modal--window')
+
+function closeModal() {
+console.log(modal)
+modal.style.display = 'none';
+}
+
+
+
